4a679f642540a0e1ef274e1919b5f2c9f267dbb3
oxycodoner <rallyfank@gmail.com>
2020-07-15 03:04:45 +0300

    Add siepam-13 task.

--- build.sbt
+++ build.sbt
@@ -2,10 +2,18 @@ name := "SparkTestApp"
 
 version := "0.1"
 
-scalaVersion := "2.11.12"
+scalaVersion := "2.12.12"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.6"
-libraryDependencies += "org.apache.spark" %% "spark-graphx" % "2.4.6"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.6"
-libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.6" % "runtime"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-graphx" % "3.0.0"
+//libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.6"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "3.0.0"
+
+//libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka" % "1.6.3"
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "3.0.0"
+//libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % "2.3.0"
+//libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.5.0"
+//libraryDependencies += "com.squareup.retrofit2" % "converter-gson" % "2.3.0"
+libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.5.0"
+
 
--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,37 +1,55 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.kafka.common.serialization.StringDeserializer
+
+
+case class Event(event_name: String, event_id: Long, time: Long)
+case class GroupTopic(urlkey: String, topic_name: String)
+case class Group(group_topics: List[GroupTopic], group_city: String, group_country: String,
+                 group_id: Long, group_name: String, group_state: String)
+case class Member(member_id: Long, member_name: String)
+case class Venue(venue_name: String, lon: Double, lat: Double, venue_id: Long)
+case class Message(venue: Venue, visibility: String, response: String, guests: Long,
+                   member: Member, rsvp_id: Long, mtime: Long, event: Event,
+                   group: Group)
 
 object Test {
   def main(args: Array[String]) {
-//    System.setProperty("hadoop.home.dir", "C:\\winutil\\")
-
-//=================batch==============================
     System.setProperty("hadoop.home.dir", "C:\\winutil\\")
 
-    val spark: SparkSession = SparkSession
-      .builder.master("local")
-      .appName("Scala Spark Example")
-      .getOrCreate()
+    val brokers = "kafkaserver:9092"
+    val groupid = "GRP1"
+    val topics = "data_test"
 
+    val sparkConf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("TestStreaming")
 
-    val flightData2015 = spark
-      .read
-      .option("inferSchema", true)
-      .option("header", true)
-      .csv("src/main/resources/2015-summary.csv")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
 
-    flightData2015.createOrReplaceTempView("flight_data_2015")
+    val sc = ssc.sparkContext
+    sc.setLogLevel("OFF")
 
-    val sqlWay = spark.sql("""
-       SELECT DEST_COUNTRY_NAME, count(1)
-       FROM flight_data_2015
-       GROUP BY DEST_COUNTRY_NAME
-       """)
+    val kafkaParam = Map[String, Object](
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> brokers,
+      ConsumerConfig.GROUP_ID_CONFIG -> groupid,
+      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer]
+    )
 
-    val dataFrameWay = flightData2015
-      .groupBy("DEST_COUNTRY_NAME")
-      .count()
-    sqlWay.explain
-    dataFrameWay.explain
-//=========================================================
+    val topicSet = Array("data_test")
+
+    val messages = KafkaUtils.createDirectStream[String, String](
+      ssc, LocationStrategies.PreferConsistent, ConsumerStrategies.Subscribe[String, String](
+        topicSet, kafkaParam
+      )
+    )
+
+    messages.map(record=>(record.value().toString)).print
+
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
